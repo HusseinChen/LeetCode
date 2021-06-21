53fcfe26eb539495c293d96df1d424484e8ecc13
hussein <43138199@qq.com>
2021-06-21 23:03:55 +0800

    test git ignore

---
+++ src/com/hussein/leetcode/easy/Test2.java
@@ -0,0 +1,15 @@
+package com.hussein.leetcode.easy;
+
+/**
+ * @Description: TODO{这里用一句话描述类的作用,若修改类,请在注释内使用cac+tab}
+ * @author: 43138
+ * @E-mail: 43138199@qq.com
+ * @date: 2021/6/21
+ * @time: 23:03
+ * 2021/6/21 23:03 43138 create
+ * @version: TODO
+ */
+public class Test2 {
+
+  // 测试git ignore
+}
